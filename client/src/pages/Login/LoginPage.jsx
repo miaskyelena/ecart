9f931586439229c6a65efc2ffdc3535b912aa0e4
W-McPhail <miaskyelena@gmail.com>
2023-12-28 21:21:45 -0500

    updates to login screen

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -7,7 +7,7 @@ import {
 } from '@supabase/auth-ui-shared'
 import  LoginImage  from '../../assets/images/boutique.jpg'
 import './LoginPage.css'
-import HomePage from '../Home/HomePage'
+
 
 const supabase = createClient('https://tgkovlzatftsifoxqxdi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRna292bHphdGZ0c2lmb3hxeGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1NTI1MTcsImV4cCI6MjAxNDEyODUxN30.5Q7ata3VlXApFGHwyW8cAuPfYHG_c7WVgsVRNSBsJys')
 
@@ -28,7 +28,10 @@ const LoginPage = () => {
     return () => subscription.unsubscribe()
   }, [])
 
+  console.log(session)
+
   return (
+    <>
     <div className='login-container'>
        <div className='login-container-item'>
             <div className='login-container-auth mt-5 w-50'>
@@ -46,6 +49,7 @@ const LoginPage = () => {
             <img src={LoginImage} alt='Authentication' className='login-image' />
         </div>
     </div>
+    </>
   )
 }
 
